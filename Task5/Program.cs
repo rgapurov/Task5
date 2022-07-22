ec1035c36b7615e4e7c8b8a1dbddf435db323c5a
rgapurov <92965000+rgapurov@users.noreply.github.com>
2022-07-22 12:57:56 +0300

    Add project files.

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5;
+
+namespace consoleBankamatik
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            fiat.Mmodel = "Punto";
+            fiat.ModelYaz();
+        }
+    }
+}
+
---
+++ Task5/opel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class opel
+    {
+        private string model;
+        public static string Mmodel = "Astra";
+
+        public void SetModel()
+        { model = Mmodel; }
+        public string GetModel()
+        { return model; }
+        public static void ModelYaz()
+        {
+            Console.WriteLine("Araba modeli: " + Mmodel);
+        }
+    }
+}
